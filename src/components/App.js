649c973ce0ab8c4ce71642b46ea8f836f97a8511
bibekkhatiwada16 <bibekkhatiwada16@gmail.com>
2021-08-02 16:27:47 +0545

    ipfs need to update soon

--- src/components/App.js
+++ src/components/App.js
@@ -35,25 +35,19 @@ class App extends Component {
     const networkData = Marketplace.networks[networkId]
     if(networkData) {
       const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
-      this.setState ({ marketplace })
-      const productCount= await marketplace.methods.productCount().call()
+      this.setState({ marketplace })
+      const productCount = await marketplace.methods.productCount().call()
       this.setState({ productCount })
-
-      //Load products
-
-      for( var i=0; i<=productCount; i++){
+      // Load products
+      for (var i = 1; i <= productCount; i++) {
         const product = await marketplace.methods.products(i).call()
         this.setState({
           products: [...this.state.products, product]
-
         })
-
       }
-
-      this.setState ({ loading: false })
-     
+      this.setState({ loading: false})
     } else {
-      window.alert('BlockchainMarketplace contract not deployed to detected network.')
+      window.alert('Marketplace contract not deployed to detected network.')
     }
   }
 
@@ -70,70 +64,42 @@ class App extends Component {
     this.purchaseProduct = this.purchaseProduct.bind(this)
   }
 
-  // CREATE A NEW FUNCTION INSIDE OUR COMPONENT TO CREATE PRODUCT
-
-  createProduct(name, price){
+  createProduct(name, price) {
     this.setState({ loading: true })
-    this.state.marketplace.methods.createProduct(name,price).send({ from:this.state.account})
+    this.state.marketplace.methods.createProduct(name, price).send({ from: this.state.account })
     .once('receipt', (receipt) => {
       this.setState({ loading: false })
-
     })
   }
-  // Create a new function to purchase product
-  purchaseProduct(id, price){
-     this.setState({
-       loading:true
-     })
-     this.state.marketplace.methods.purchaseProduct(id).send({ from: this.state.account, value: price })
-     .once('receipt',(receipt) => {
 
-      this.setState({loading: false})
-
-     })
-
-
+  purchaseProduct(id, price) {
+    this.setState({ loading: true })
+    this.state.marketplace.methods.purchaseProduct(id).send({ from: this.state.account, value: price })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
   }
-
-
 
   render() {
     return (
       <div>
-
         <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
-          <div className = "row">
-            <main role = "main" className="col-lg-12 d-flex">
-              { this.state.loading 
-                ? <div id= "loader" className="text-center"><p className="text-center">Loading...</p></div>
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex">
+              { this.state.loading
+                ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
                 : <Main
-                   products={ this.state.products }  
-                   createProduct={this.createProduct} 
-                   purchaseProduct={this.purchaseProduct} /> 
+                  products={this.state.products}
+                  createProduct={this.createProduct}
+                  purchaseProduct={this.purchaseProduct} />
               }
-
             </main>
           </div>
         </div>
-
-
-        
       </div>
     );
   }
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-      
-
-        
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow" class="navbar navbar-dark bg-primary">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
           href="https://www.facebook.com/blockchainnepal2009"
