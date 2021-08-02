314390a18582025ed4db7f110b6ea42492f171f5
bibekkhatiwada16 <bibekkhatiwada16@gmail.com>
2021-08-02 21:33:31 +0545

    navbar edit....

---
+++ build/precache-manifest.a196985fd250c803f96e459b3b576961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d6978273280d51bc62a6",
+    "url": "/static/js/main.d6978273.chunk.js"
+  },
+  {
+    "revision": "25edf1b67ffd49f1d504",
+    "url": "/static/js/1.25edf1b6.chunk.js"
+  },
+  {
+    "revision": "d6978273280d51bc62a6",
+    "url": "/static/css/main.a757c263.chunk.css"
+  },
+  {
+    "revision": "25edf1b67ffd49f1d504",
+    "url": "/static/css/1.636848e5.chunk.css"
+  },
+  {
+    "revision": "6ef1d7a368f50417b7d60b293af75985",
+    "url": "/index.html"
+  }
+];
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ class Navbar extends Component {
         </a>
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-            <small className="text-white"><span id="account">{this.props.account}</span></small>
+            <small className="text-white"><span id="account">Current Logged In Address:<br></br>{this.props.account}</span></small>
           </li>
         </ul>
       </nav>
